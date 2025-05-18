2a678972713b721f41215a634ec17e70c33c36cf
Maksim Yarotskiy <maksimyarotskiy08@gmail.com>
2025-05-18 12:39:29 +0300

    Initial commit: Spring Boot project for SellerScope

---
+++ src/main/java/com/sellerscope/SellerScopeApplication.java
@@ -0,0 +1,15 @@
+package com.sellerscope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellerScopeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SellerScopeApplication.class, args);
+	}
+
+	
+
+}

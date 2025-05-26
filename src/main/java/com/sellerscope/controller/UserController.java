f9f1723c6d260421ffcbec637a4fb8e77f22aa71
Maksim Yarotskiy <maksimyarotskiy08@gmail.com>
2025-05-27 00:18:44 +0300

    Base work with user entity

---
+++ src/main/java/com/sellerscope/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.sellerscope.controller;
+
+import com.sellerscope.dto.RegisterRequest;
+import com.sellerscope.entity.User;
+import com.sellerscope.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<User> register(@RequestBody RegisterRequest request) {
+        User user = userService.register(request);
+        return ResponseEntity.ok(user);
+    }
+}
---
+++ src/main/java/com/sellerscope/entity/User.java
@@ -0,0 +1,24 @@
+package com.sellerscope.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "users", uniqueConstraints = @UniqueConstraint(columnNames = "email"))
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+
+    private String password;
+}
